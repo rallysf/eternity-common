569ff2ef39ded7b0c2d96cc3835abf49fe432e5f
Michael Gregson <michael.gregson@rally.org>
2014-05-01 12:28:27 -0700

    Remove unused import and bring in the GeneratedMessage-to-JSON stuff

--- src/main/java/com/eternity/common/message/RPCCommand.java
+++ src/main/java/com/eternity/common/message/RPCCommand.java
@@ -6,7 +6,6 @@ import java.nio.ByteBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.eternity.common.annotations.BindCommand;
 import com.eternity.common.communication.protocol.Decoder;
 import com.eternity.common.communication.protocol.Encoder;
 import com.eternity.common.exceptions.EternityException;
